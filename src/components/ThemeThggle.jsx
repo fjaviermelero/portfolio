a1462db93a2bf854e8bea5598dbd27aa74ce2649
unknown <fjaviermelero@hotmail.com>
2025-08-26 20:08:49 +0200

    first commit

---
+++ src/components/ThemeThggle.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { Sun, Moon } from 'lucide-react';
+
+export const ThemeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+
+    if (storedTheme === 'dark') {
+      setIsDarkMode(true);
+      document.documentElement.classList.add('dark');
+    } else {
+      setIsDarkMode(false);
+      document.documentElement.classList.remove('dark');
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (isDarkMode) {
+      // Si está en oscuro → pásalo a claro
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+      setIsDarkMode(false);
+    } else {
+      // Si está en claro → pásalo a oscuro
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+      setIsDarkMode(true);
+    }
+  };
+
+  return (
+    <button onClick={toggleTheme}>
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-blue-600" />
+      )}
+    </button>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { ThemeToggle } from "../components/ThemeThggle";
+
+export const Home = () => {
+    return (
+    <div className='min-h-screen container bg-background text-foreground overflow-x-hidden'>
+        {/* Theme Toggle */}
+        <ThemeToggle />
+
+        {/* Background Effect */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+
+    </div>
+    );
+};
